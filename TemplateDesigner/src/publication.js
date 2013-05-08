1d98e25fa0fcdb92c4bdf80441c49da2325a1a22
Dave Springgay <sprnggy67@gmail.com>
2013-05-08 11:17:35 +0100

    Adding styling for gridCell

--- TemplateDesigner/src/publication.js
+++ TemplateDesigner/src/publication.js
@@ -16,7 +16,7 @@ ds.Publication = function() {
 ds.Publication.prototype.loadFromServer = function(callback) {
 	// Load the data.
 	this.name = "The National";
-	this.defaultTemplate = defaultTemplates.empty; // front3 contains a full template.
+	this.defaultTemplate = defaultTemplates.front3; // Use empty or front3
 
 	// Invoke the callback.
 	callback();
